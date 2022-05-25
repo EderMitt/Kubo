79a90ac8c956882421dcfc3437682124dfd77ca8
Eder Mittelstadt <edermitt@outlook.com>
2022-05-24 21:58:30 -0300

    Primeira atualização

---
+++ Rotator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private Touch touch;
+    [SerializeField] private Vector2 touchPosition;
+    [SerializeField] private Quaternion rotation;
+
+    private float rotateSpeedModifier = 0.2f;
+    
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                rotation = Quaternion.Euler(0f, - touch.deltaPosition.x * rotateSpeedModifier, 0f);
+
+                transform.rotation = rotation * transform.rotation;
+                
+                if (transform.localEulerAngles.y <= 0f)
+                {
+                    Debug.Log("Deu certo");
+                }
+            }
+        }
+    }
+}
